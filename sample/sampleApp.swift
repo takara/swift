fe13cbf41009ee5b82dd2ba62f4e3213ed064e57
TAKA <taka2063jp@gmail.com>
2022-12-10 16:32:16 +0900

    first commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by takara on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
